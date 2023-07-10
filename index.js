4dd81793c2d81a520c721c719d9d116f72e14eb3
Benjamin Lewis <ben.lewis@visimedia.co.uk>
2023-07-10 12:45:10 +0100

    Refactor code for shutdown functionality and update package details
    - Refactored the code to fix a typo in the import statement, changing "paltform" to "platform".
    - Renamed the file "paltform.js" to "platform.js".
    - Updated the package name from "shutdown_computer" to "systemshutdown".
    - Updated the repository URL in the package.json file.
    - Changed the author name from "Zaheer ud Din Faiz" to "VisiMedia".
    - Updated the bugs and homepage URLs in the package.json file.
    
    These changes improve code readability, fix a typo, and update package details.

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const { Platform } = require("./paltform");
+const { Platform } = require("./platform");
 const cp = require("child_process");
 function Shutdown() {
-  this.currentPlatform = new Platform();
+	this.currentPlatform = new Platform();
 
-  this.schedualeShutdown = (time) => {
-    if (time.sec === undefined) time.sec = 0;
-    if (time.min === undefined) time.min = 0;
-    if (time.hour === undefined) time.hour = 0;
+	this.scheduleShutdown = (time) => {
+		if (time.sec === undefined) time.sec = 0;
+		if (time.min === undefined) time.min = 0;
+		if (time.hour === undefined) time.hour = 0;
 
-    let delayTime =
-      time.sec * 1000 + time.min * 60 * 1000 + time.hour * 60 * 60 * 10000;
+		let delayTime =
+			time.sec * 1000 + time.min * 60 * 1000 + time.hour * 60 * 60 * 10000;
 
-    setTimeout(() => {
-      this.initShutdown();
-    }, delayTime);
+		setTimeout(() => {
+			this.initShutdown();
+		}, delayTime);
 
-    console.log(
-      `Shutdown schedualed for  ${time.hour} hour ${time.min} min ${time.sec} sec  `
-    );
-  };
+		console.log(
+			`Shutdown scheduled for  ${time.hour} hour ${time.min} min ${time.sec} sec  `
+		);
+	};
 
-  this.initShutdown = () => {
-    return new Promise((resolve, reject) => {
-      let shutdownCommand = this.getCommandShutdown();
-      cp.exec(shutdownCommand, function (err, stdout, stderr) {
-        if (err) return reject(err.message);
-        resolve("Shutting Down");
-      });
-    });
-  };
-  this.getCommandShutdown = () => {
-    switch (this.currentPlatform.platform.toLowerCase()) {
-      case "darwin":
-      case "linux":
-        return "sudo shutdown -h now";
-      case "win32":
-        return "shutdown -s -t 0";
-    }
-    return false;
-  };
+	this.initShutdown = () => {
+		return new Promise((resolve, reject) => {
+			let shutdownCommand = this.getCommandShutdown();
+			cp.exec(shutdownCommand, function (err, stdout, stderr) {
+				if (err) return reject(err.message);
+				resolve("Shutting Down");
+			});
+		});
+	};
+	this.getCommandShutdown = () => {
+		switch (this.currentPlatform.platform.toLowerCase()) {
+			case "linux":
+				return "sudo shutdown -h now";
+			case "win32":
+				return "shutdown -s -t 0";
+			default:
+				throw new Error("Platform not supported");
+		}
+	};
 }
 
 module.exports.Shutdown = Shutdown;
